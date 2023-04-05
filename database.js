834b31975b8c2887a9defd86132ba889094f4ed2
Cindy <cindy@Cindys-MacBook-Pro.local>
2023-04-04 22:33:03 -0400

    removed unused get functions and reviews table+trigger.

--- database.js
+++ database.js
@@ -1,23 +1,19 @@
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database(":memory:");
+//Connect to DB
+// const db = new sqlite3.Database('./test.db', sqlite3.OPEN_READWRITE, (err)=>{
+//   if (err) return console.error(err.message);
+// })
 
 db.serialize(() => {
-  db.run('CREATE TABLE professors (id INTEGER PRIMARY KEY AUTOINCREMENT, blockchainID INTEGER UNIQUE NOT NULL, name TEXT NOT NULL)');
-  db.run('CREATE TABLE university (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL)');
-  db.run('CREATE TABLE course (id INTEGER PRIMARY KEY AUTOINCREMENT, code TEXT NOT NULL, name TEXT NOT NULL)');
-  db.run('CREATE TABLE teaching (id INTEGER PRIMARY KEY AUTOINCREMENT, professorID INTEGER REFERENCES professors(id), universityID INTEGER REFERENCES university(id), courseID TEXT REFERENCES course(id))');
+  db.run('CREATE TABLE IF NOT EXISTS professors (id INTEGER PRIMARY KEY AUTOINCREMENT, blockchainID INTEGER UNIQUE NOT NULL, name TEXT NOT NULL)');
+  db.run('CREATE TABLE IF NOT EXISTS university (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL)');
+  db.run('CREATE TABLE IF NOT EXISTS course (id INTEGER PRIMARY KEY AUTOINCREMENT, code TEXT NOT NULL, name TEXT NOT NULL)');
+  db.run('CREATE TABLE IF NOT EXISTS teaching (id INTEGER PRIMARY KEY AUTOINCREMENT, professorID INTEGER REFERENCES professors(id), universityID INTEGER REFERENCES university(id), courseID TEXT REFERENCES course(id))');
   //db.run('CREATE TABLE reviews (id INTEGER PRIMARY KEY AUTOINCREMENT, professorID INTEGER REFERENCES professors(id), university TEXT NOT NULL, rating INTEGER, review TEXT)');
   // Trigger checks rating is between 0 and 10 before inserting new row
-  db.run('CREATE TRIGGER check_rating BEFORE INSERT ON reviews WHEN NEW.rating < 0 OR NEW.rating > 10 BEGIN SELECT RAISE(ABORT, "Rating must be between 0 and 10"); END;');
+  //db.run('CREATE TRIGGER IF NOT EXISTS check_rating BEFORE INSERT ON reviews WHEN NEW.rating < 0 OR NEW.rating > 10 BEGIN SELECT RAISE(ABORT, "Rating must be between 0 and 10"); END;');
 });
-
-// DROP TABLES
-// db.run("DROP TABLE IF EXISTS professors")
-// db.run("DROP TABLE IF EXISTS university")
-// db.run("DROP TABLE IF EXISTS course")
-// db.run("DROP TABLE IF EXISTS teaching")
-//("DROP TABLE IF EXISTS reviews")
-
 
 // INSERT Data
 db.addProfessor = (blockchainID, profName) => {
@@ -60,15 +56,46 @@ db.addTeaching = (professorID, universityID, course) => {
     return success.changes > 0 ? true : false;
 }
 
-// db.addReview = (professorId, university, rating, reviewText) => {
-//   let success = false;
-//     try {
-//       success = db.run("INSERT INTO REVIEWS (professorID, university, rating, review) VALUES (?, ?, ?, ?)", [professorId, university, rating, reviewText]);
-//     } catch (dbError) {
-//       console.error(dbError);
-//     }
-//     return success.changes > 0 ? true : false;
-//   }
+// UPDATE Data
+db.updateProfessor = (id, blockchainID, name) => {
+  let success = false;
+  try {
+    success = db.run("UPDATE PROFESSORS SET blockchainID = ?, name = ? WHERE id = ?", [blockchainID, name, id]);
+  } catch (dbError) {
+    console.error(dbError);
+  }
+  return success.changes > 0 ? true : false;
+}
+
+db.updateUniversity = (id, name) => {
+  let success = false;
+  try {
+    success = db.run("UPDATE University SET name = ? WHERE id = ?", [name, id]);
+  } catch (dbError) {
+    console.error(dbError);
+  }
+  return success.changes > 0 ? true : false;
+}
+
+db.updateCourse = (id, code, name) => {
+  let success = false;
+  try {
+    success = db.run("UPDATE course SET code = ?, name = ? WHERE id = ?", [name, code, id]);
+  } catch (dbError) {
+    console.error(dbError);
+  }
+  return success.changes > 0 ? true : false;
+}
+
+db.updateTeaching = (id, professorID, universityID, courseID) => {
+  let success = false;
+  try {
+    success = db.run("UPDATE teaching SET professorID = ?, universityID = ?, courseID = ? WHERE id = ?", [ professorID, universityID, courseID, id]);
+  } catch (dbError) {
+    console.error(dbError);
+  }
+  return success.changes > 0 ? true : false;
+}
 
 // DELETE Data
 db.deleteProf = (id) => {
@@ -111,61 +138,10 @@ db.deleteTeaching = (id) => {
   return success.changes > 0 ? true : false;
 }
 
-// db.deleteReview = (id) => {
-//   let success = false;
-//   try {
-//     success = db.run("Delete from REVIEWS WHERE id = ?", [id]);
-//   } catch (dbError) {
-//     console.error(dbError);
-//   }
-//   return success.changes > 0 ? true : false;
-// }
-
-// UPDATE Data
-db.updateProfessor = (id, blockchainID, name) => {
-  let success = false;
-  try {
-    success = db.run("UPDATE PROFESSORS SET blockchainID = ?, name = ? WHERE id = ?", [blockchainID, name, id]);
-  } catch (dbError) {
-    console.error(dbError);
-  }
-  return success.changes > 0 ? true : false;
-}
-
-db.updateUniversity = (id, name) => {
-  let success = false;
-  try {
-    success = db.run("UPDATE University SET name = ? WHERE id = ?", [name, id]);
-  } catch (dbError) {
-    console.error(dbError);
-  }
-  return success.changes > 0 ? true : false;
-}
-
-db.updateCourse = (id, code, name) => {
-  let success = false;
-  try {
-    success = db.run("UPDATE course SET code = ?, name = ? WHERE id = ?", [name, code, id]);
-  } catch (dbError) {
-    console.error(dbError);
-  }
-  return success.changes > 0 ? true : false;
-}
-
-db.updateTeaching = (id, professorID, universityID, courseID) => {
-  let success = false;
-  try {
-    success = db.run("UPDATE teaching SET professorID = ?, universityID = ?, courseID = ? WHERE id = ?", [ professorID, universityID, courseID, id]);
-  } catch (dbError) {
-    console.error(dbError);
-  }
-  return success.changes > 0 ? true : false;
-}
-
 // GET Data 
 // get all columns from university table
 db.getUniversities = () => {
-  db.all("SELECT id, name FROM university", (err, rows) => {
+  db.all("SELECT * FROM university", (err, rows) => {
     if (err) {
       console.error("Error getting professor from the database:", err);
       return rows;
@@ -178,7 +154,9 @@ db.getUniversities = () => {
 // get professor id, professor blockchainID, professor name 
 // for professors who have taught at the universityid inputed
 db.getProfessors = (universityID) => {
-  db.all("SELECT professor.id, professor.blockchainID, professor.name FROM professors INNER JOIN teaching ON professor.id = teaching.professorID WHERE teaching.universityID = ?", [universityID], (err, rows) => {
+  db.all("SELECT professor.id, professor.blockchainID, professor.name FROM professors INNER JOIN teaching ON professor.id = teaching.professorID WHERE teaching.universityID = ?", 
+  [universityID], 
+  (err, rows) => {
     if (err) {
       console.error("Error getting professor from the database:", err);
       return rows;
@@ -200,78 +178,49 @@ db.getAllProfessors = () => {
   });
 };
 
-// get Reviews for a professor by professor id
-// db.getReviews = (id) => {
-//   db.all("SELECT professors.name, reviews.university, reviews.rating, reviews.review FROM reviews JOIN professors ON reviews.professorID = professors.id WHERE professors.schoolID = ?", [schoolID], (err, rows) => {
-//     if (err) {
-//       console.error("Error getting reviews from database:", err);
-//       return rows;
-//     } else {
-//       return rows;
-//     }
-//   });
-// };
+// DROP TABLES
+db.dropProfessors = () => {
+  db.all("DROP TABLE IF EXISTS professors", (err, rows) => {
+    if (err) {
+      console.error("Error dropping professors table from from database:", err);
+      return rows;
+    } else {
+      return rows;
+    }
+  });
+};
 
-// get all columns from professors table where id matches input id
-// db.getProfessors = (id) => {
-//   db.all("SELECT * FROM professors WHERE id = ?", [id], (err, rows) => {
-//     if (err) {
-//       console.error("Error getting professor from the database:", err);
-//       return rows;
-//     } else {
-//       return rows;
-//     }
-//   });
-// };
+db.dropUniversity = () => {
+  db.all("DROP TABLE IF EXISTS university", (err, rows) => {
+    if (err) {
+      console.error("Error dropping university table from database:", err);
+      return rows;
+    } else {
+      return rows;
+    }
+  });
+};
 
+db.dropCourse = () => {
+  db.all("DROP TABLE IF EXISTS course", (err, rows) => {
+    if (err) {
+      console.error("Error dropping course table from database:", err);
+      return rows;
+    } else {
+      return rows;
+    }
+  });
+};
 
-// // Search by professor name
-// db.getProfessorName = (name) => {
-//   db.all("SELECT * FROM professors WHERE name = ?", [name], (err, rows) => {
-//     if (err) {
-//       console.error("Error getting professor from the database:", err);
-//       return rows;
-//     } else {
-//       return rows;
-//     }
-//   });
-// };
+db.dropTeaching = () => {
+  db.all("DROP TABLE IF EXISTS teaching", (err, rows) => {
+    if (err) {
+      console.error("Error dropping teaching table from database:", err);
+      return rows;
+    } else {
+      return rows;
+    }
+  });
+};
 
-
-// // Search by professor schoolID
-// db.getProfessorSchoolID = (schoolID) => {
-//   db.all("SELECT * FROM professors WHERE schoolID = ?", [schoolID], (err, rows) => {
-//     if (err) {
-//       console.error("Error getting professor from the database:", err);
-//       return rows;
-//     } else {
-//       return rows;
-//     }
-//   });
-// };
-
-// // Search professors by university 
-// db.getProfessorSchoolID = (univname) => {
-//   db.all("SELECT * FROM teaching JOIN professors ON teaching.professorID = professors.ID WHERE university.name = ?", [univname], (err, rows) => {
-//     if (err) {
-//       console.error("Error getting professor from the database:", err);
-//       return rows;
-//     } else {
-//       return rows;
-//     }
-//   });
-// };
-
-// // Search by professor name, schoolID
-// db.getProfessorSchoolID = (name, schoolID) => {
-//   db.all("SELECT * FROM professors WHERE name = ? AND schoolID = ?", [name, schoolID], (err, rows) => {
-//     if (err) {
-//       console.error("Error getting professor from the database:", err);
-//       return rows;
-//     } else {
-//       return rows;
-//     }
-//   });
-// };
-
-module.exports = { db, getReviews };
+module.exports = { db };
