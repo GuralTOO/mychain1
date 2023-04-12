0fa72c246012293257b725634e73d7e4f574503e
naander <125314789+naander@users.noreply.github.com>
2023-04-12 18:05:42 -0400

    server.js with Nate's changes

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+// Nate changes: there are different exports now
+const {
+  // updateDatabaseOnUserUpdated,
+  addReview,
+  getReviews
+} = require("./contract");
+
+
 const {
   contract,
   updateDatabaseOnUserUpdated,
@@ -10,6 +18,7 @@ const {
   addReview,
 } = require("./contract");
 const db = require("./database");
+
 
 app.use(cors());
 app.use(express.json());
@@ -20,17 +29,25 @@ app.listen(3001, () => {
 });
 
 //propogate changes from the blockchain to the database
+// Nate changes: are we still doing this or no?
 updateDatabaseOnUserUpdated();
 
 //receive a request to create a new review
 app.post("/api/addReview", async (req, res) => {
   // Add review to blockchain contract
+  // Nate changes: should this line be this instead
+  // const { profID, review, rating } = req.body;
   const { userId, wallet } = req.body;
+  
+  // Nate changes: Don't need either of these since there in contract.js
   const fromAddress = "0x..."; // Replace with the address that will send the transaction
   const privateKey = "0x..."; // Replace with the private key of the fromAddress
 
+  // Nate changes: the addReview function takes different parameters
+  // const result = await addReview(profID, review, rating);
   const result = await addReview(userId, wallet, fromAddress, privateKey);
 
+  // Nate changes: purpose of this?? Were not dealing with the transaction hash
   if (result.success) {
     res
       .status(200)
@@ -40,9 +57,12 @@ app.post("/api/addReview", async (req, res) => {
   }
 });
 
+// Nate changes: the front end just needs to send a profID for this function, idk what :id is doing
 //receive a request to get all reviews for a professor
 app.get("/api/getReviews/:id", async (req, res) => {
   const id = req.params.id;
+  
+  // Nate changes: we are no longer using the db to hold reviews/ratings
   // Get reviews from the SQLite database
   try {
     const reviews = await db.getReviews(id);
